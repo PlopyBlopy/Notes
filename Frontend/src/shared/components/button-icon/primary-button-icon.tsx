05d9f77b51428af13f9ca0b3900e689a12379c2e
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2025-12-06 23:24:54 +0300

    refactor(primary-button-icon, F): изменено формирование стиля их typed значений, перенесены стили из style в className

--- Frontend/src/shared/components/button-icon/primary-button-icon.tsx
+++ Frontend/src/shared/components/button-icon/primary-button-icon.tsx
@@ -2,21 +2,20 @@ import { Icons } from "@/shared/assets/icons";
 import styles from "./primary-button-icon.module.css";
 
 export interface Style {
-  color?: string;
+  variant?: "light" | "greyDarker" | "accent" | "danger"; // Вместо color
 }
 
 interface Props extends Style {
   onClick: () => void;
   IconComponent?: React.ComponentType<{ className?: string }>;
+  label: string;
 }
 
-export const ButtonIcon = ({ onClick, IconComponent = Icons.default, color }: Props) => {
-  const style: React.CSSProperties = {
-    color: color,
-  };
+export const ButtonIcon = ({ onClick, IconComponent = Icons.default, label, variant = "greyDarker" }: Props) => {
+  const variantClass = styles[variant] || styles.greyDarker;
 
   return (
-    <button style={style} className={styles.button} onClick={onClick}>
+    <button title={label} className={`${styles.button} ${variantClass}`} onClick={onClick}>
       <IconComponent className={styles.icon} />
     </button>
   );
